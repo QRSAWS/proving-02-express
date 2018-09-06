564df338e79ac7ba7bc8302f109db7beb055b04e
Andrew Stephenson <AStephenson@QRS.LOCAL>
2018-09-06 14:41:35 -0500

    fixed line 14 clerical error

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 //Lets require/import the HTTP module
 var http = require('http');
+var fs = require('fs');
  //Lets define a port we want to listen to
 var PORT=8080;
  //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
+ 
+
+ 	fs.readFile("index.html", function(err, data){
+	  res.writeHead(200, {'Content-Type': 'text/html'});
+	  res.end(data);
+	});
+};
+
  //Create a server
 var server = http.createServer(handleRequest);
  //Lets start our server
